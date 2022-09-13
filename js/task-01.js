1438eed1889bae2245007e4787d79a52a9e27fed
Taras <tarasbohun07@gmail.com>
2022-09-13 12:55:19 +0300

    done task-01

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+const totalCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${totalCategories.length}`);
+console.log("");
+
+const categories = document.querySelectorAll(".item");
+
+for (let i = 0; i < categories.length; i += 1) {
+  const titleOfCategory = categories[i].firstElementChild.textContent;
+  const totalEl = categories[i].querySelectorAll("li").length;
+
+  console.log(`Category: ${titleOfCategory}`);
+  console.log(`Elements: ${totalEl}`);
+  console.log("");
+}
+// -------------------- forEach ---------------------
+// const result = categories.forEach((category) => {
+//   const titleOfCategory = category.firstElementChild.textContent;
+//   const totalEl = category.querySelectorAll("li").length;
+
+//   console.log(`Category: ${titleOfCategory}`);
+//   console.log(`Elements: ${totalEl}`);
+//   console.log("");
+// });
