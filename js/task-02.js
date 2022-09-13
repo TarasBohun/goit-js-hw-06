bd40ff74ab39df5b17ab774a32b909b590e83529
Taras <tarasbohun07@gmail.com>
2022-09-13 13:38:22 +0300

    done task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,32 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const oldList = document.querySelector("#ingredients");
+
+const result = ingredients.forEach((ingredient) => {
+  const newEl = document.createElement("li");
+  newEl.classList.add("item");
+  newEl.textContent = ingredient;
+
+  oldList.append(newEl);
+});
+
+// for (const ingredient of ingredients) {
+//   const newEl = document.createElement("li");
+//   newEl.classList.add("item");
+//   newEl.textContent = ingredient;
+//   oldList.append(newEl);
+// }
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const newEl = document.createElement("li");
+//   newEl.classList.add("item");
+//   newEl.textContent = ingredients[i];
+//   oldList.append(newEl);
+// }
