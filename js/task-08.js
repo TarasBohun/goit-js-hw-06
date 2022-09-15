a97f38dda3b6437dd3ff69a5bd18ef7502561e87
Taras <tarasbohun07@gmail.com>
2022-09-15 22:14:05 +0300

    done task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
+// console.log(form);
 
+form.addEventListener("submit", handleSubmit);
+
+const object = {};
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Заповніть усі поля!!!");
+  }
+
+  object.email = email.value;
+  object.password = password.value;
+  console.log(object);
+
+  event.currentTarget.reset();
+}
